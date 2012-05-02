5efc784f3b3f15d4828db510cdc45868000df493
Timmy O'Mahony <me@timmyomahony.com>
2012-05-02 16:36:42 +0200

    Improved post install script to move contents of pagedown static media

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ from os import path
 from setuptools import setup
 from subprocess import check_call
 from distutils.command.build import build
+from distutils.dir_util import copy_tree, remove_tree
 
 class build_with_submodules(build):
     def run(self):
         if path.exists('.git'):
             check_call(['git', 'submodule', 'init'])
             check_call(['git', 'submodule', 'update'])
+            # Move contents of pagedown and remove .git
+            dst = "pagedown/static/pagedown/"
+            src = "pagedown/static/pagedown/pagedown/"
+            copy_tree(src, dst)
+            remove_tree(src)
+            os.remove("pagedown/static/pagedown/.git")
+            os.remove("pagedown/static/pagedown/README.md")
         build.run(self)
 
 
