ab63d5aa3b01047192884071ccf723bb6261fb9e
Timmy O'Mahony <t.omahony.dublin@gmail.com>
2012-10-29 01:11:04 +0100

    Peppified

--- pagedown/widgets.py
+++ pagedown/widgets.py
@@ -2,21 +2,23 @@ from django import forms
 from django.conf import settings
 from django.contrib.admin import widgets as admin_widgets
 from django.forms.widgets import flatatt
-from django.utils.html import escape, conditional_escape
+from django.utils.html import conditional_escape
 from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
+
 
 class PagedownWidget(forms.Textarea):
     class Media:
         css = {
-            'all' : ('pagedown/demo/browser/demo.css',)
+            'all': ('pagedown/demo/browser/demo.css',)
         }
         js = ('%s/pagedown/Markdown.Converter.js' % settings.STATIC_URL,
               '%s/pagedown/Markdown.Sanitizer.js' % settings.STATIC_URL,
               '%s/pagedown/Markdown.Editor.js' % settings.STATIC_URL,)
-    
+
     def render(self, name, value, attrs=None):
-        if value is None: value = ''
+        if value is None:
+            value = ''
         if 'class' not in attrs:
             attrs['class'] = ""
         attrs['class'] += " wmd-input"
@@ -34,7 +36,7 @@ class PagedownWidget(forms.Textarea):
                     var converter = Markdown.getSanitizingConverter();
                     selectors = {
                         input : "%(id)s",
-                        button : "%(id)s_wmd_button_bar", 
+                        button : "%(id)s_wmd_button_bar",
                         preview : "%(id)s_wmd_preview",
                     }
                     var editor = new Markdown.Editor(converter, selectors);
@@ -42,16 +44,15 @@ class PagedownWidget(forms.Textarea):
                 })();
             </script>
             """ % {
-                'attrs' : flatatt(final_attrs),
-                'body' : conditional_escape(force_unicode(value)),
-                'id' : attrs['id'],
+                'attrs': flatatt(final_attrs),
+                'body': conditional_escape(force_unicode(value)),
+                'id': attrs['id'],
             }
         return mark_safe(html)
-        
+
+
 class AdminPagedownWidget(admin_widgets.AdminTextareaWidget, PagedownWidget):
     class Media:
         css = {
-            'all' : ('admin/css/pagedown.css',)
+            'all': ('admin/css/pagedown.css',)
         }
-    
-    
