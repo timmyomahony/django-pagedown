9c035619e148eac8a493572aa645ca686966f6bc
Timmy O'Mahony <me@timmyomahony.com>
2013-06-06 16:53:00 +0200

    Updated setup.py for better submodule integration and PEP'ed everything

--- setup.py
+++ setup.py
@@ -1,41 +1,37 @@
-import os
 from os import path
 from setuptools import setup
 from subprocess import check_call
 from distutils.command.build import build
-from distutils.dir_util import copy_tree, remove_tree
 from setuptools.command.develop import develop
 
-def get_submodules_and_fix_paths():
+
+def get_submodules():
     if path.exists('.git'):
         check_call(['rm', '-rf', 'pagedown/static/pagedown'])
         check_call(['git', 'reset', '--hard'])
         check_call(['git', 'submodule', 'init'])
         check_call(['git', 'submodule', 'update'])
-        # Move contents of pagedown and remove .git
-        dst = "pagedown/static/pagedown/"
-        src = "pagedown/static/pagedown/pagedown/"
-        copy_tree(src, dst)
-        remove_tree(src)
+
 
 class build_with_submodules(build):
     def run(self):
-        get_submodules_and_fix_paths()
+        get_submodules()
         build.run(self)
+
 
 class develop_with_submodules(develop):
     def run(self):
-        get_submodules_and_fix_paths()
+        get_submodules()
         develop.run(self)
 
 
 setup(
-  name = "django-pagedown",
-  version = "0.0.3",
-  author = "Timmy O'Mahony",
-  author_email = "me@timmyomahony.com",
-  url = "https://github.com/timmyomahony/django-pagedown",
-  description = ("A django app that allows the easy addition of Stack Overflow's 'PageDown' markdown editor to a django form field"),
+  name="django-pagedown",
+  version="0.0.3",
+  author="Timmy O'Mahony",
+  author_email="me@timmyomahony.com",
+  url="https://github.com/timmyomahony/django-pagedown",
+  description=("A django app that allows the easy addition of Stack Overflow's 'PageDown' markdown editor to a django form field"),
   long_description=open('README.md').read(),
   packages=['pagedown'],
   include_package_data=True,
