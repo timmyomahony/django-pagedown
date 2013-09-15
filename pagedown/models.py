0424e91aa3824c9ddc3908b94cc8f5db7feaa209
Adam LeVasseur <adam.j.lev@gmail.com>
2013-09-15 15:17:32 -0400

    South introspection rule for PagedownField

--- pagedown/models.py
+++ pagedown/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class PagedownField(models.TextField):
     pass
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^pagedown\.models\.PagedownField"])
+except ImportError:
+    pass
