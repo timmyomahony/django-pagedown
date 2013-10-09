d19c43a60feac9de117e8c07e098594b95233a28
Timmy O'Mahony <me@timmyomahony.com>
2013-10-10 00:27:03 +0200

    Added introspection rules to the new form fields

--- pagedown/forms.py
+++ pagedown/forms.py
@@ -10,3 +10,11 @@ class PagedownField(forms.CharField):
 class AdminPagedownField(forms.CharField):
     ''' A simple CharField that allows us avoid having to write widget code '''
     widget = AdminPagedownWidget
+
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^pagedown\.forms\.PagedownField"])
+    add_introspection_rules([], ["^pagedown\.forms\.AdminPagedownField"])
+except ImportError:
+    raise
