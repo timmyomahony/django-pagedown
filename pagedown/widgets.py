78bbb8d8b6a307ef0f3381cb849a6f8467354a3a
Timmy O'Mahony <hey@timmyomahony.com>
2015-12-11 16:34:47 +0000

    Fixed mismatching quotes

--- pagedown/widgets.py
+++ pagedown/widgets.py
@@ -18,44 +18,44 @@ except ImportError:  # python3
 class PagedownWidget(forms.Textarea):
 
     def __init__(self, *args, **kwargs):
-        self.show_preview = kwargs.pop('show_preview', pagedown_settings.SHOW_PREVIEW)
-        self.template = kwargs.pop('template', pagedown_settings.WIDGET_TEMPLATE)
-        self.css = kwargs.pop('css', pagedown_settings.WIDGET_CSS)
+        self.show_preview = kwargs.pop("show_preview", pagedown_settings.SHOW_PREVIEW)
+        self.template = kwargs.pop("template", pagedown_settings.WIDGET_TEMPLATE)
+        self.css = kwargs.pop("css", pagedown_settings.WIDGET_CSS)
         super(PagedownWidget, self).__init__(*args, **kwargs)
 
     def _media(self):
         return forms.Media(
             css={
-                'all': self.css
+                "all": self.css
             },
             js=(
-                compatible_staticpath('pagedown/Markdown.Converter.js'),
-                compatible_staticpath('pagedown/Markdown.Sanitizer.js'),
-                compatible_staticpath('pagedown/Markdown.Editor.js'),
-                compatible_staticpath('pagedown_init.js'),
+                compatible_staticpath("pagedown/Markdown.Converter.js"),
+                compatible_staticpath("pagedown/Markdown.Sanitizer.js"),
+                compatible_staticpath("pagedown/Markdown.Editor.js"),
+                compatible_staticpath("pagedown_init.js"),
             ))
     media = property(_media)
 
     def render(self, name, value, attrs=None):
         if value is None:
-            value = ''
+            value = ""
         final_attrs = self.build_attrs(attrs, name=name)
-        if 'class' not in final_attrs:
-            final_attrs['class'] = ""
-        final_attrs['class'] += " wmd-input"
+        if "class" not in final_attrs:
+            final_attrs["class"] = ""
+        final_attrs["class"] += " wmd-input"
         return render_to_string(self.template, {
-            'attrs': flatatt(final_attrs),
-            'body': conditional_escape(force_unicode(value)),
-            'id': final_attrs['id'],
-            'show_preview': self.show_preview,
+            "attrs": flatatt(final_attrs),
+            "body": conditional_escape(force_unicode(value)),
+            "id": final_attrs["id"],
+            "show_preview": self.show_preview,
         })
 
 
 class AdminPagedownWidget(PagedownWidget, admin_widgets.AdminTextareaWidget):
     class Media:
         css = {
-            'all': (compatible_staticpath('admin/css/pagedown.css'),)
+            "all": (compatible_staticpath("admin/css/pagedown.css"),)
         }
         js = (
-            compatible_staticpath('admin/js/pagedown.js'),
+            compatible_staticpath("admin/js/pagedown.js"),
         )
