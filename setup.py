5743661271e4b4cc2eed15a4b0037ab85d641cbd
Timmy O'Mahony <hey@timmyomahony.com>
2015-12-11 16:59:30 +0000

    Changed email address

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
   name="django-pagedown",
   version="0.1.0",
   author="Timmy O'Mahony",
-  author_email="me@timmyomahony.com",
+  author_email="hey@timmyomahony.com",
   url="https://github.com/timmyomahony/django-pagedown",
   description=("A django app that allows the easy addition of Stack Overflow's 'PageDown' markdown editor to a django form field"),
   long_description=open('README.md').read(),
