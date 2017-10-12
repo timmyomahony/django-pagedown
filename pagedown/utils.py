713724a1d8d093cc69ddc86ee2fa47aa42542909
Timmy O'Mahony <hey@timmyomahony.com>
2017-10-12 14:37:07 +0100

    Cleaned up comments

--- pagedown/utils.py
+++ pagedown/utils.py
@@ -3,9 +3,8 @@ from django.conf import settings
 
 def compatible_staticpath(path):
     '''
-    Try to return a path to static the static files compatible all
-    the way back to Django 1.2. If anyone has a cleaner or better
-    way to do this let me know!
+    Try to return a path the static files compatible all the way back to Django
+    1.2. If anyone has a cleaner or better way to do this let me know.
     '''
     try:
         # >= 1.4
