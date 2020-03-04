435d41683af9dd8150fec5db9e90fdf8a44c5a25
Michał Bielawski <michal@bielaw.ski>
2020-03-04 11:13:14 +0000

    Add Django 3.0 and Python 3.8 as supported versions.

--- setup.py
+++ setup.py
@@ -50,12 +50,14 @@ setup(
         'Topic :: Text Editors',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ]
 )
