23659cdf26d256d5ee0bfafa84d3a551808a9a3b
Markus Thielen <mt@thiguten.de>
2020-12-18 12:44:07 +0100

    add widget parameter to override upload path

--- pagedown/widgets.py
+++ pagedown/widgets.py
@@ -6,16 +6,18 @@ from django.contrib.admin import widgets
 class PagedownWidget(forms.Textarea):
     template_name = 'pagedown/forms/widgets/default.html'
 
-    def __init__(self, attrs=None):
+    def __init__(self, attrs=None, upload_dir=None):
         super(PagedownWidget, self).__init__(attrs=attrs)
         # Add wmd-input class for easier styling
         self.attrs['class'] = '{} wmd-input'.format(
             self.attrs.get('class', ''))
+        self.upload_dir = upload_dir
 
     def get_context(self, name, value, attrs):
         context = super(PagedownWidget, self).get_context(name, value, attrs)
         context["image_upload_enabled"] = getattr(
             settings, 'PAGEDOWN_IMAGE_UPLOAD_ENABLED', False)
+        context["upload_dir"] = self.upload_dir
         return context
 
     class Media:
